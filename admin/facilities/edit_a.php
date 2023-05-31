0a0ad7b4ba2255bd0df9e83a4d7bf39fadd1f078
Nihaya2002 <126665306+Nihaya2002@users.noreply.github.com>
2023-05-31 21:30:07 +0700

    revisi tambah data

--- admin/facilities/edit_a.php
+++ admin/facilities/edit_a.php
@@ -5,19 +5,13 @@ $id=$_GET['id'];
 $query = mysqli_query($koneksi, "SELECT*FROM facilities WHERE id='$id'");
 
 while ($facilities = mysqli_fetch_array($query)){
-    $id = $facilities['id'];
     $facility_name = $facilities['facility_name'];
     $tour_id = $facilities['tour_id'];
-    $created_at = $facilities['created_at'];
-    $updated_at = $facilities['updated_at'];
     
 }
 ?>
 <form action="proses_edit_a.php?id=<?php echo $id ?>" method="post">
 <table>
-<tr>    <td>Id</td>
-    <td><input type = "text" name= "id" value="<?php echo $id; ?> "></td>
-</tr>
 <tr>
     <td>Facility Name</td>
     <td><input type = "text" name= "facility_name" value="<?php echo $facility_name; ?> "></td>
@@ -26,13 +20,6 @@ while ($facilities = mysqli_fetch_array($query)){
     <td>Tour Id</td>
     <td><input type = "text" name= "tour_id" value="<?php echo $tour_id; ?> "></td>
 </tr>
-<tr>
-    <td>Created At</td>
-    <td><input type = "text" name= "created_at" value="<?php echo $created_at; ?> "></td>
-</tr>
-<tr>
-    <td>Updated At</td>
-    <td><input type = "text" name= "updated_at" value="<?php echo $updated_at; ?> "></td>
 <tr>
     <td colspan=2><input type="submit" name ="submit" value="Ubah"></td>
 </tr>
--- admin/facilities/proses_edit_a.php
+++ admin/facilities/proses_edit_a.php
@@ -3,13 +3,10 @@
 include_once("koneksi_a.php");
 $id = $_GET["id"];
 
-$id = $_POST['id'];
 $facility_name = $_POST['facility_name'];
 $tour_id = $_POST['tour_id'];
-$created_at= $_POST['created_at'];
-$updated_at = $_POST['updated_at'];
 
-$result = mysqli_query($koneksi, "UPDATE `facilities` SET id = '$id', facility_name = '$facility_name', tour_id = '$tour_id', created_at = '$created_at', updated_at = '$updated_at' WHERE id ='$id';");
+$result = mysqli_query($koneksi, "UPDATE `facilities` SET facility_name = '$facility_name', tour_id = '$tour_id' WHERE id ='$id';");
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
