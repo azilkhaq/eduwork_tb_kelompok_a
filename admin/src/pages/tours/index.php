739de02c237038a24e44f4def62ea01b1df2225e
azilkhaq <azilkhaq10@gmail.com>
2023-06-02 16:38:36 +0700

    change Tours to Tour in title content

--- admin/src/pages/tours/index.php
+++ admin/src/pages/tours/index.php
@@ -21,12 +21,12 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="page-title-box d-flex align-items-center justify-content-between">
-                                <h4 class="page-title mb-0 font-size-18">Tours</h4>
+                                <h4 class="page-title mb-0 font-size-18">Tour</h4>
 
                                 <div class="page-title-right">
                                     <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Tours</a></li>
-                                        <li class="breadcrumb-item active">Tours</li>
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Tour</a></li>
+                                        <li class="breadcrumb-item active">Tour</li>
                                     </ol>
                                 </div>
 
