7255addc0b81e9ed540a316b09f048412fb72cb2
Nihaya2002 <126665306+Nihaya2002@users.noreply.github.com>
2023-06-04 18:29:18 +0700

    delete folder facilities

--- admin/src/pages/facility/process_add.php
+++ admin/src/pages/facility/process_add.php
@@ -2,10 +2,10 @@
 $facility_name=$_POST["facility_name"];
 $tour_id=$_POST["tour_id"];
 
-include ('koneksi_a.php');
+include ('connection.php');
 
 $query= mysqli_query ($koneksi,"INSERT INTO `facilities` (`facility_name`,`tour_id`) 
 VALUES ('$facility_name','$tour_id');");
 
-header("Location:index_a.php")
+header("Location:index.php")
 ?>
