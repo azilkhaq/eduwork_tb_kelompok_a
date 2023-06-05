13b3180343bac67358d89885744e362348205f4f
azilkhaq <azilkhaq10@gmail.com>
2023-06-05 20:30:42 +0700

    tambah data tours

---
+++ admin/config/database.php
@@ -0,0 +1,7 @@
+<?php
+$host       =   "localhost";
+$user       =   "root";
+$password   =   "";
+$database   =   "traveling";
+
+$koneksi = mysqli_connect($host, $user, $password, $database);
--- admin/src/pages/tours/edit.php
+++ admin/src/pages/tours/edit.php
@@ -25,12 +25,12 @@ $query = mysqli_query($koneksi, "SELECT * FROM categories");
                     <div class="row">
                         <div class="col-12">
                             <div class="page-title-box d-flex align-items-center justify-content-between">
-                                <h4 class="page-title mb-0 font-size-18">Tambah Wisata</h4>
+                                <h4 class="page-title mb-0 font-size-18">Edit Wisata</h4>
 
                                 <div class="page-title-right">
                                     <ol class="breadcrumb m-0">
                                         <li class="breadcrumb-item"><a href="javascript: void(0);">Wisata</a></li>
-                                        <li class="breadcrumb-item active">Tambah Wisata</li>
+                                        <li class="breadcrumb-item active">Edit Wisata</li>
                                     </ol>
                                 </div>
 
@@ -44,7 +44,7 @@ $query = mysqli_query($koneksi, "SELECT * FROM categories");
                             <div class="card">
                                 <div class="card-body">
 
-                                    <h4 class="card-title mb-4">Form Tambah Wisata</h4>
+                                    <h4 class="card-title mb-4">Form Edit Wisata</h4>
 
                                     <h4 class="card-title">Gambar Wisata</h4>
                                     <p class="card-title-desc">Upload file drag'n'drop dengan pratinjau gambar.
